e6dd04fd0ff3ec69143549eec1c1cf37742b86cb
Harvey Zack <laozhaochaguan@gmail.com>
2019-05-07 15:38:45 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # blur-image-url
+
+## Install
+
+Install with `npm`
+
+```
+$ npm install blur-image-url
+```
+
+Or install with `yarn`
+
+```
+$ yarn add stackBlurToUrl
+```
+
+```js
+import blurImageUrl from 'blur-image-url';
+```
+
+Or umd builds are also available
+
+```html
+<script src="path/to/blur-image-url.js"></script>
+```
+
+Will expose the global variable to `window.blurImageUrl`.
+
+## Usage
+
+```js
+// Only need image address and blur radius
+blurImageUrl('./banner.png', 100).then(url => {
+    console.log(url);
+});
+```
+
+## License
+
+MIT © [Harvey Zack](https://sleepy.im/)
---
+++ index.js
@@ -0,0 +1,38 @@
+(function(window, factory) {
+  if (typeof exports === 'object') {
+    module.exports = factory();
+  } else if (typeof define === 'function' && define.amd) {
+    define(factory);
+  } else {
+    window.blurImageUrl = factory();
+  }
+})(this, function() {
+  return function blurImageUrl(url, radius) {
+    return new Promise(function(resolve) {
+      var img = new Image();
+      img.onload = function() {
+        var w = img.naturalWidth;
+        var h = img.naturalHeight;
+        var canvas = document.createElement('canvas');
+        canvas.style.width = w + 'px';
+        canvas.style.height = h + 'px';
+        canvas.width = w;
+        canvas.height = h;
+        var context = canvas.getContext('2d');
+        context.filter = `blur(${radius}px)`;
+        context.drawImage(
+          img,
+          -radius,
+          -radius,
+          w + radius * 2,
+          h + radius * 2
+        );
+        canvas.toBlob(function(blob) {
+          resolve(URL.createObjectURL(blob));
+          img.onload = null;
+        });
+      };
+      img.src = url;
+    });
+  };
+});
